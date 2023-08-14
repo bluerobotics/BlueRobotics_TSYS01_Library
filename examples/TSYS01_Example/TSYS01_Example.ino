9caaf12a73b47a130d3bfce50a793ae07cfe4ee9
ES-Alexander <sandman.esalexander@gmail.com>
2023-08-14 20:54:55 +1000

    examples: update TSYS01_Example
    - include new initialisation validation
    - whitespace formatting update

--- examples/TSYS01_Example/TSYS01_Example.ino
+++ examples/TSYS01_Example/TSYS01_Example.ino
@@ -36,32 +36,26 @@ THE SOFTWARE.
 -------------------------------*/
 
 #include <Wire.h>
-
 #include "TSYS01.h"
 
 TSYS01 sensor;
 
 void setup() {
-  
-  Serial.begin(9600);
-  
-  Serial.println("Starting");
-  
-  Wire.begin();
+    Serial.begin(9600);
+    Serial.println("Starting");
 
-  sensor.init();
-
+    Wire.begin();
+  
+    while (!sensor.init()) {
+        Serial.println("TSYS01 device failed to initialize!");
+        delay(2000);
+    }
 }
 
 void loop() {
-
-  sensor.read();
-
-  Serial.print("Temperature: ");
-  
-  Serial.print(sensor.temperature()); 
-  
-  Serial.println(" deg C");
-
-  delay(1000);
+    sensor.read();
+    Serial.print("Temperature: ");
+    Serial.print(sensor.temperature()); 
+    Serial.println(" deg C");
+    delay(1000);
 }
